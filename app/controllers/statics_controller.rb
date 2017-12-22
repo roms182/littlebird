ea6ca7ecf0c85c5a4fe4772becd02e5c0cb8b261
Romain Despature <romain@9troisquarts.com>
2017-12-22 13:53:15 +0100

    add cloudinary api

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,4 +1,7 @@
 class StaticsController < ApplicationController
+  require 'open-uri'
+  require 'json'
   def index
+    @images = JSON.load(open("http://res.cloudinary.com/roms182/image/list/littlebird.json"))
   end
 end
